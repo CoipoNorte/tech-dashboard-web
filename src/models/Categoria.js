c0d9a263712cbb5a5b8280dc4b22ccce31000f06
CoipoNorte <christiancaceres1398@gmail.com>
2025-08-12 15:01:36 -0400

    Actualizacion 7

--- src/models/Categoria.js
+++ src/models/Categoria.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const categoriaSchema = new mongoose.Schema({
-  nombre: { type: String, required: true, unique: true }
+  nombre: { type: String, required: true, unique: true },
+  icono: { type: String, default: 'bi bi-tag' }
 });
 
 module.exports = mongoose.model('Categoria', categoriaSchema);
--- src/models/Estado.js
+++ src/models/Estado.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const estadoSchema = new mongoose.Schema({
   nombre: { type: String, required: true, unique: true },
-  color: { type: String, required: true, default: '#6c757d' } // gris Bootstrap
+  color: { type: String, required: true, default: '#6c757d' },
+  icono: { type: String, default: 'bi bi-flag' }
 });
 
 module.exports = mongoose.model('Estado', estadoSchema);
